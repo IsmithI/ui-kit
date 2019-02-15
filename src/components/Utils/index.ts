41576027581ab22ee47877b0c22531b43f3ad4ed
Oleg Bondar <olegbondar1998@gmail.com>
2019-02-15 20:52:54 +0200

    Added export members

---
+++ src/components/Utils/index.ts
@@ -0,0 +1,3 @@
+export * from './AnimatedMount';
+export * from './DelayedUnmount';
+export * from './Portal';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './Animation';
 export * from './Button';
+export * from './Card';
